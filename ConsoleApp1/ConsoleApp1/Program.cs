102d9f91f90c993d547251de077d12a0a06a4c6c
Helen <helen7bren@gmail.com>
2018-09-06 14:38:36 +1200

    prac test 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("--------This program can tell you if your number is odd or even----------");
+            Console.WriteLine("--------------------------------------------------------------------------");
+
+            int i = 0;
+
+
+
+
+            while (i <= 1000)
+            {
+                Console.WriteLine("Enter a whole number between 1 and 1000");
+                int input = int.Parse(Console.ReadLine());
+
+                if (input % 2 == 0)
+                {
+                    Console.WriteLine(" Your number is even");
+
+
+                }
+                else
+                {
+                    Console.WriteLine(" Your number is odd");
+                }
+
+
+
+
+
+
+
+
+
+
+                Console.WriteLine("----------------------------------------------------------------------");
+                Console.WriteLine("--------This program will compute the average of ten numbers-----------");
+                Console.WriteLine("-----------------------------------------------------------------------");
+
+                float total =
+                int a = 1;
+
+                while (a <= 10)
+                {
+                    Console.WriteLine("enter number ");
+                    num = int.Parse(Console.ReadLine());
+
+                    a++;
+
+
+                    Console.WriteLine("The average of those 10 numbers is: ")
+                }
+
+
+
+
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine("-----------This program will expand a basic quadratic equation--------");
+                Console.WriteLine("-----------------------   (x+n) (x+m)  --------------------------------");
+
+                Console.WriteLine("Looking at the above brackets....");
+
+                Console.WriteLine("Enter a +ve or -ve value for n:+5");
+
+            }
+
+            Console.ReadLine();
+
+
+
+
+                
+                
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+            
+           
+            
+
+        }
+    }
+}
